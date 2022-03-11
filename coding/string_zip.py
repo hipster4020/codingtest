b4eed7f8a5294106dd734e32ca09ada102f46775
hipster4020 <hipster4020@gmail.com>
2022-03-11 08:21:49 +0000

    문자열 압축

--- coding/string_zip.py
+++ coding/string_zip.py
@@ -15,25 +15,33 @@
 
 # different code
 def solution(s):
-    answer = 10000
-    for n in range(1, len(s)//2+2):
-        res = ""
+    answer = len(s)
+    # 1개 단위(step)부터 압축 단위를 늘려가며 확인
+    for step in range(1, len(s) // 2 + 1) :
+        compressed = ""
+        prev = s[0:step] # 앞에서부터 step만큼의 문자열 추출
         count = 1
-        temp = s[:n]
         
-        for i in range(n, len(s)+n, n):
-            if temp == s[i:i+n]:
+        #단위(step) 크기만큼 증가시키며 이전 문자열과 비교
+        for j in range(step, len(s), step) :
+            #이전 상태와 동일하다면 압축 횟수(count) 증가
+            if prev == s[j:j + step] :
                 count += 1
-            else:
-                if count == 1:
-                    res += temp
-                else:
-                    res += str(count) + temp
-                temp = s[i:i+n]
+            # 다른 문자열이 나왔다면
+            else :
+                compressed += str(count) + prev if count >= 2 else prev
+                
+                # 다시 상태 초기화
+                prev = s[j:j + step]
                 count = 1
-    print(res)
-    answer = min(answer, len(res))
-    print(answer)
+                
+        # 남아 있는 문자열에 대해서 처리
+        compressed += str(count) + prev if count >= 2 else prev
+        # 만들어지는 압축 문자열이 가장 짧은 것이 정답
+        answer = min(answer, len(compressed))
+
+    return answer
+
     return answer
 if __name__ == "__main__":
     s = "aabbaccc"
